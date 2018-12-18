53608a20fa5ee7e0e5d127cbc7319bf6c8286a96
acollierr17 <gamemaniac101@gmail.com>
2018-12-17 21:27:11 -0500

    Add ability to get the current prefix by simply mentioning the bot

--- events/message.js
+++ events/message.js
@@ -7,6 +7,9 @@ module.exports = async (client, message) => {
     const prefixMention = new RegExp(`^<@!?${client.user.id}> `);
     const newPrefix = message.content.match(prefixMention) ? message.content.match(prefixMention)[0] : prefix;
 
+    const getPrefix = new RegExp(`^<@!?${client.user.id}>( |)$`);
+    if (message.content.match(getPrefix)) return message.channel.send(`My prefix in this guild is \`${prefix}\``);
+
     if (message.author.bot) return;
     if (message.content.indexOf(newPrefix) !== 0) return;
 
