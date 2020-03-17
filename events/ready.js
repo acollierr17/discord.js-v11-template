289d084eae5cdb3bef3022615074e8d568dbef53
Mu7aMMaD-4411 <46306277+Mu7aMMaD-4411@users.noreply.github.com>
2020-03-17 03:42:12 +0100

    guild's in stats
    Make the stats show how many servers the bot are in

--- events/ready.js
+++ events/ready.js
@@ -1,20 +1,14 @@
 const { prefix } = require('../config');
 const { version } = require('../package.json');
 
-const versions = {
-    production: 'Production',
-    development: 'Development'
-};
-
 module.exports = async client => {
 
-    await client.logger.log(`Logged in as ${client.user.tag} (${client.user.id}) in ${client.guilds.size} server(s).`);
-    await client.logger.log(`Version ${version} of the bot loaded.`);
-    await client.logger.log(`${versions[process.env.NODE_ENV]} version of the bot loaded.`);
+  await client.logger.log(`[ Name: ${client.user.tag} ].`);
+  await client.logger.log(`[ Version: ${version} ].`);
     
     const cmdHelp = client.commands.get('help', 'help.name');
-    
+   
     client.user.setStatus('online');
-    client.user.setActivity(`${prefix + cmdHelp}`, { type: 'PLAYING' })
+    client.user.setActivity(`${client.guilds.size} guild's | ${prefix + cmdHelp}`, { type: 'PLAYING' })
         .catch(err => client.logger.error(err));
 };
