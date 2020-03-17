ea56ed51012fcffb34e8878f9cf0230d96385cb4
Mu7aMMaD-4411 <46306277+Mu7aMMaD-4411@users.noreply.github.com>
2020-03-17 03:54:02 +0100

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -1,12 +1,11 @@
 exports.run = async (client, message, args) => {
-
-    const msg = await message.channel.send('Ping?');
-    msg.edit(`Pong! Latency is \`${msg.createdTimestamp - message.createdTimestamp}ms\`. API Latency is \`${Math.round(client.ping)}ms\`.`);
+  const msg = await message.channel.send("🔔 Pinging..");
+  msg.edit(`📶 Round Trip & Response: \`${Date.now() - message.createdTimestamp} Millisecond's\`. \n💻 Discord Application Programming Interface: \`${Math.round(client.ping)} Millisecond's\`.`);
 };
 
 exports.help = {
-    name: 'ping',
-    aliases: [],
-    description: 'View the latency of the bot and API.',
-    usage: 'ping'
+  name: "ping",
+  aliases: ["pong", "pinga"],
+  description: "View the latency of the client and API.",
+  usage: "ping"
 };
